42d8241292c7aa6412ae81d1a81b485acaad30e1
Maurine-Jebet <maurinejebett@gmai.com>
2023-03-15 13:10:04 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,90 @@
+fun main() {
+println(myName(arrayOf("jane","june","juliet","janet")))
+    typesOfDrinks(6)
+    typesOfDrinks(7)
+    typesOfDrinks(40)
+     numList()
+    numbers()
+
+}
+
+
+fun typesOfDrinks(age: Int){
+    when (age){
+        0,6 -> println("milk")
+        7,14 -> println ("fanta orange")
+        else -> println("coca cola")
+    }
+}
+
+
+
+fun numList(){
+    for (nums in 1..100){
+        if (nums % 2 !=0){
+            println(nums)
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Create a function that prints out all the odd numbers between 1 and 100 (2pts)
+//2. Create a function that takes in an array of names and returns the number of
+//names longer than 5 characters (2pts)
+fun myName(word:Array <String>) : Int {
+    var count = 0
+    for (y in word) {
+        if (y.length > 5) {
+            count++
+        }
+    }
+    return count
+}
+//3. You are creating a robot to serve drinks at a party. The robot is required to
+//serve a glass of milk for guests under the age of 6. For guests under the age of
+//15 but over the age of 6, the robot serves a bottle of fanta orange. Everybody
+//else gets a bottle of. Create a function that will make these
+//decisions, printing out the name of the drink each person gets according to
+//their age. (2pts)
+//4. Write a function that prints each number from 1 to 100 on a new line. For each
+//multiple of 3, print "Fizz" instead of the number. For each multiple of 5, print
+//"Buzz" instead of the number. For numbers which are multiples of both 3 and
+//5, print "FizzBuzz" instead of the number. (4pts)
+
+
+fun numbers(){
+    for (num in 1 .. 100)
+    if (num % 3 == 0&&num % 5 == 0){
+        println("FizzBuzz")
+    }
+    else if (num % 3 == 0){
+        println("Fizz")
+    }
+    else if (num % 5 == 0){
+        println("Buzz")
+    }
+    else{
+        println(num)
+    }
+}
